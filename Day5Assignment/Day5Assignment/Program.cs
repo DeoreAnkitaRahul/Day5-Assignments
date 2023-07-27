2f5dbc6faf135ea0032e7ba788b36b183188b0f9
“DeoreAnkitaRahul” <ankitapatil375@gmail.com>
2023-07-27 16:42:28 +0530

    [Ankita] Add : UC1-Find Min For 3 Integer

---
+++ Day5Assignment/Day5Assignment/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day5Assignment
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            MinInteger minimumInteger = new MinInteger();
+            minimumInteger.IntegerMethod();
+            Console.ReadLine();
+        }
+    }
+}
